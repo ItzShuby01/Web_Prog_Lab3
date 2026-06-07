1dbbcdad9ef4cf6f13e25549d1e2ad3cd62142db
Shu'aibu Idris <shuaibuidrisshubi261@gmail.come>
2026-06-07 03:06:03 +0300

    APACHE ANT for LAB3 Software Eng. Basics (ОПИ)

---
+++ src/main/java/web/MainApp.java
@@ -0,0 +1,8 @@
+package web;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("JSF requires no main method, just for ANT ENV target testing purpose.");
+        System.out.println("JVM parameters and constrained environment OK.");
+    }
+}
